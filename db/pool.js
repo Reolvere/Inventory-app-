4e1b58b1906d0697328888f205a3f1666ffb3af0
Serhii <111197050+Reolvere@users.noreply.github.com>
2025-03-15 14:35:07 -0600

    commit2

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,7 @@
+require("dotenv").config()
 const { Pool } = require("pg")
 
 module.exports = new Pool ({
-    connectionString: "postgresql://root:root1@localhost:5432/inventory"
+    // connectionString: "postgresql://root:root1@localhost:5432/inventory"
+    connectionString: process.env.DATABASE_URL
 })
